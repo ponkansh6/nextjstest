9f1508bf8450a7ae6b838fa054bcb889175a6587
s.hara <menonaki2@gmail.com>
2026-06-17 08:52:01 +0900

    fix: pre-commit hook to skip failing type-check

--- vite.zero.config.ts
+++ vite.zero.config.ts
@@ -1,12 +1,11 @@
-
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [],
   build: {
     lib: {
-      entry: 'tests/minimal-speed.test.ts',
-      formats: ['es'],
+      entry: "tests/minimal-speed.test.ts",
+      formats: ["es"],
     },
     minify: false,
   },
